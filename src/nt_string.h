38c70b1dccb65e032a571959830acec29166a846
Jeff Anderson <jeff.anderson@digecor.com>
2016-09-28 08:47:48 -0600

    Remove cruft

--- src/nt_string.h
+++ src/nt_string.h
@@ -5,19 +5,6 @@
 #define NT_XOR_SWAP(a,b) a ^= b; b ^= a; a ^= b; 
 
 
-/*
-do\
-{\
-	a ^= b;\
-	b ^= a;\
-	a ^= b;\
-} while (0)
-
-*/
-
-
-
-
 char* nt_chop(char* s);
 char* nt_chomp(char* s);
 char* nt_strrev(char* s);
