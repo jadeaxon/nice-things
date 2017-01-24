71ce962fe36d2ab63f60658f6347561c1aa74dda
Jeff Anderson <jeff.anderson@digecor.com>
2017-01-24 09:34:29 -0700

    Build library in Cygwin
    For some of these hello programs it's nice to have the lib in Cygwin
    because running my Ubuntu VM can slow the rest of the computer down
    significantly.  May be time to buy a new laptop.

--- build.sh
+++ build.sh
@@ -1,16 +1,50 @@
+#!/usr/bin/env bash
+
 set -e
 
 cd src >& /dev/null
 
-gcc -c -fPIC nt_*.c
-gcc -shared -o libnicethings.so nt_*.o
-cp libnicethings.so $HOME/lib
+S=$(basename $0)
 
-gcc test_nt_string.c -Wl,-rpath,$HOME/lib -L. -lnicethings -I. -o test_nt_string
-gcc test_nt_crypt.c -Wl,-rpath,$HOME/lib -L. -lnicethings -I. -o test_nt_crypt
-gcc test_nt_error.c -Wl,-rpath,$HOME/lib -L. -lnicethings -I. -o test_nt_error
-gcc test_nt_signal.c -Wl,-rpath,$HOME/lib -L. -lnicethings -I. -o test_nt_signal
+lib=nicethings
+fpic='-fPIC' # Arg to generate position independent code.
+if [ "$CYGWIN" ]; then
+	echo "$S: Building for Cygwin."	
+	libfile=lib${lib}.dll.a
+	fpic='' # All Windows code is position independent.
+else
+	echo "$S: Building for Linux."
+	libfile=lib${lib}.so
+fi
 
+b=../build
+rm -rf $b
+mkdir -p $b
+
+# Build and deploy the library itself.
+# Deployment path for the library.
+dlibpath=$HOME/lib
+gcc -c $fpic nt_*.c
+gcc -shared -o $b/$libfile nt_*.o
+if [ ! -d $dlibpath ]; then
+	echo "$S: ERROR: $dlibpath is not a directory!" 1>&2
+	exit 1
+fi
+cp $b/$libfile $dlibpath
+rm *.o
+
+# The rpath mechanism allows program to find lib in non-standard location without
+# setting LD_LIBRARY_PATH.
+rpath="-Wl,-rpath,$dlibpath"
+
+# Build the test scripts.
+# TO DO: For each loop.
+gcc test_nt_string.c $rpath -L. -l$lib -I. -o $b/test_nt_string
+gcc test_nt_crypt.c $rpath -L. -l$lib -I. -o $b/test_nt_crypt
+gcc test_nt_error.c $rpath -L. -l$lib -I. -o $b/test_nt_error
+gcc test_nt_signal.c $rpath -L. -l$lib -I. -o $b/test_nt_signal
+
+ls -la $b
 cd - >& /dev/null
 
 
