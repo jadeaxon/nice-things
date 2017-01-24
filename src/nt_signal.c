3b53b82555b70465ab4b9ee16dc3a04348844fe8
Jeff Anderson <jeff.anderson@digecor.com>
2017-01-23 22:31:10 -0700

    Comment on how to restore default signal handler

--- src/nt_signal.c
+++ src/nt_signal.c
@@ -1,10 +1,13 @@
 #include "nt_signal.h"
 
-#include <signal.h> // sigaction(), sigemptyset(), struct sigaction
+#include <signal.h> // sigaction(), sigemptyset(), struct sigaction, SIGINT, etc.
 #include <stdlib.h> // NULL
+
 
 // Registers a signal handler for the given signal.
 // Returns -1 if it fails.
+//
+// Use nt_register_signal_handler(SIGINT, SIG_DFL) to restore default handler.
 int nt_register_signal_handler(int signal, void (*handler)(int)) {
 	struct sigaction action;
 	action.sa_handler = handler;
