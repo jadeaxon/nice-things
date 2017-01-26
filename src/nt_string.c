4f0f46001c51781e008084b93fe2e583b804964a
Jeff Anderson <jeff.anderson@digecor.com>
2017-01-25 21:15:17 -0700

    Comment out debug output; prune dead code

--- src/nt_string.c
+++ src/nt_string.c
@@ -57,19 +57,15 @@ char* nt_strrev(char* str) {
 // Keeps only chars in s that exist in keepers.  Deletes all other characters.
 // Mutates the given string and returns it.
 char* nt_keep_chars(char* s, char* keepers) {
-	printf("+nt_keep_chars(%s, %s)\n", s, keepers);
-	// char* temp = strdup(s);
-	// char* i = temp;
-	// char* s0 = s;
+	// printf("+nt_keep_chars(%s, %s)\n", s, keepers);
 	char* i = s;
 	char* s0 = s;
 	while (*s != '\0') {
 		char c = *s;
 		char* k = keepers;
-		printf("Checking %c\n", c);
-		// int keep = 0;
+		// printf("Checking %c\n", c);
 		while (*k++ != '\0') {
-			printf("  %c == %c?\n", c, *k);
+			// printf("  %c == %c?\n", c, *k);
 			if (c == *k) {
 				*i = c;
 				i++;
@@ -80,7 +76,7 @@ char* nt_keep_chars(char* s, char* keepers) {
 		s++;
 	} // next string char
 	*i = '\0';
-	printf("-nt_keep_chars(%s, %s)\n", s0, keepers);
+	// printf("-nt_keep_chars(%s, %s)\n", s0, keepers);
 	return s0;
 } // nt_keep_chars()
 
