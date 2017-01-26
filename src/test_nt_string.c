bb10c5cc16401f95fb8d3682071abbe2177559a1
Jeff Anderson <jeff.anderson@digecor.com>
2017-01-26 09:31:16 -0700

    Use ternary op

--- src/test_nt_string.c
+++ src/test_nt_string.c
@@ -12,55 +12,29 @@ int main(void) {
 	char s[] = "A string\nto chomp\n\r";
 	char e[] = "A string\nto chomp";
 	nt_chomp(s);
-	// printf("'%s'\n", s);	
-	if (strcmp(s, e) == 0) {
-		puts("PASS");
-	}
-	else {
-		puts("FAIL");
-	}
+	(strcmp(s, e) == 0) ? puts("PASS") : puts ("FAIL");
 
 	char s2[] = "Just a string with a newline\n";
 	char e2[] = "Just a string with a newline";
 	nt_chomp(s2);
-	if (strcmp(s2, e2) == 0) {
-		puts("PASS");
-	}
-	else {
-		puts("FAIL");
-	}
+	(strcmp(s2, e2) == 0) ? puts("PASS") : puts ("FAIL");
 
 	puts("nt_chop()");
 	char s3[] = "String to chop";
 	char e3[] = "String to cho";
 	nt_chop(s3);
-	if (strcmp(s3, e3) == 0) {
-		puts("PASS");
-	}
-	else {
-		puts("FAIL");
-	}
+	(strcmp(s3, e3) == 0) ? puts("PASS") : puts ("FAIL");
 
 	puts("nt_strrev()");
 	char s4[] = "Reverse me!";
 	char e4[] = "!em esreveR";
 	nt_strrev(s4);
-	if (strcmp(s4, e4) == 0) {
-		puts("PASS");
-	}
-	else {
-		puts("FAIL");
-	}
+	(strcmp(s4, e4) == 0) ? puts("PASS") : puts ("FAIL");
 
 	char s5[] = "Even";
 	char e5[] = "nevE";
 	nt_strrev(s5);
-	if (strcmp(s5, e5) == 0) {
-		puts("PASS");
-	}
-	else {
-		puts("FAIL");
-	}
+	(strcmp(s5, e5) == 0) ? puts("PASS") : puts ("FAIL");
 
 	test__nt_keep_chars();
 
