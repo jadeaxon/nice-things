a06af1b4b2f07568e8136cf3796583f834181382
Jeff Anderson <jeff.anderson@digecor.com>
2017-01-28 17:13:34 -0700

    Add another StringDatabase test

--- src/test_nt_string_db.c
+++ src/test_nt_string_db.c
@@ -7,18 +7,29 @@
 // Tests the functions declared in nt_string_db.h.
 int main(void) {
 	puts("nt_create_string_db()");
-	puts("nt_string_db_open()");
-	puts("nt_string_db_put()");
-	puts("nt_string_db_get()");
 	StringDatabase* sdb = nt_create_string_db();
+	puts("PASS");	
+	
 	// char v[sdb->MAX_STRING_SIZE];
-	char* v = NULL;
+	puts("nt_string_db_open()");
 	sdb->open(sdb, "test.db");
+	puts("PASS");
+
+	puts("nt_string_db_put()");
+	char* v = NULL;
 	char k[80] = "exit()";
 	char e[1024] = "stdlib";
 	sdb->put(sdb, k, e);
+	puts("PASS");
+	sdb->put(sdb, "mykey", "myvalue");
+	puts("PASS");
+
+	puts("nt_string_db_get()");
 	v = sdb->get(sdb, k);
 	(strcmp(v, e) == 0) ? puts("PASS") : puts("FAIL");
+
+	v = sdb->get(sdb, "mykey");
+	(strcmp(v, "myvalue") == 0) ? puts("PASS") : puts("FAIL");
 
 	return 0;
 } // main()
